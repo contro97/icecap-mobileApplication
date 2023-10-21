cb729f1e76f7e235afb10a203e6f1a4088cce614
Ciaran Mahon <ciaranmahon97@gmail.com>
2023-10-21 16:30:57 -0700

    session list component

--- src/components/SessionsOutput/SessionsList.js
+++ src/components/SessionsOutput/SessionsList.js
@@ -1,5 +1,19 @@
-function SessionsList(){
+import { FlatList } from 'react-native';
 
+import SessionItem from './SessionItem';
+
+function renderSessionItem(itemData){
+    return <SessionItem {...itemData.item} />
+}
+
+function SessionsList({ sessions }){
+    return(
+        <FlatList 
+            data={sessions}
+            renderItem={renderSessionItem}
+            keyExtractor={(item) => item.id}
+        />
+    )
 }
 
 export default SessionsList;
