67b4d1990fa4e4479d37fae8d384221e8fc06060
Ciaran Mahon <ciaranmahon97@gmail.com>
2023-11-21 20:10:29 -0800

    return token

--- src/screens/LoginFlow/SignupScreen.tsx
+++ src/screens/LoginFlow/SignupScreen.tsx
@@ -1,17 +1,21 @@
-import {useState} from 'react';
+import { useState, useContext } from 'react';
 import {Alert} from 'react-native';
 import AuthContent from '../../components/Auth/AuthContent';
 import LoadingOverlay from '../../components/ui/LoadingOverlay';
 import { createUser } from '../../../util/auth';
+import {AuthContext} from '../../state/auth-context';
 
 
 function SignupScreen() {
   const[isAuthenticating, setIsAuthenticating] = useState(false); // initially false since unauthenticated on signup screen
 
+  const authCtx = useContext(AuthContext);
+
   async function signupHandler({email, password} : {email: string, password: string}) {
     setIsAuthenticating(true);
     try {
       await createUser(email, password);
+      authCtx.authenticate();
     }
     catch (error) {
       Alert.alert("Signup Failed", 'Could not create your account. Please check your credentials and try again.');
--- src/state/auth-context.js
+++ src/state/auth-context.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react';
 
-export const  AuthContext = createContext({
+export const AuthContext = createContext({
     token: '',
     isAuthenticated:  false,
     authenticate : () => {},
